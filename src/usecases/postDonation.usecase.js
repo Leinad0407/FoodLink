c0583a2131093d6f236f46d618fd3483919b74ce
Alan Guerrero García <age_allison@hotmail.com>
2022-07-10 15:34:39 -0500

    Se completan los endpoints para CRUD de donaciones

--- src/usecases/postDonation.usecase.js
+++ src/usecases/postDonation.usecase.js
@@ -6,14 +6,32 @@ async function createDonation(donation) {
   await Donation.create(newDonation);
 }
 
-async function getDonations() {
-  const donations = fetch("xxxx");
-  const parsedDonations = JSON.parse(donations);
+async function getDonations(filter) {
+  // const donations = fetch("xxxx");
+  const Donations = await Donation.find(filter);
+  return Donations;
+}
+async function updateDonation(id, DonationInfo) {
+  const filter = {
+    _id: id,
+  };
+  await Donation.findOneAndUpdate(filter, DonationInfo);
+  const updatedDonation = Donation.findOne(filter);
+  return updatedDonation;
+}
 
-  return parsedDonations;
+async function deleteDonation(id) {
+  const filter = {
+    _id: id,
+  };
+  await Donation.findByIdAndDelete(filter);
+  const Donations = await Donation.find(filter);
+  return Donations;
 }
 
 module.exports = {
   createDonation,
   getDonations,
+  updateDonation,
+  deleteDonation,
 };
