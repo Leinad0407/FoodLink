a464156f0862015252a4af2c178d83bcf8511366
Daniel Aguilar <ing.aguilardan@gmail.com>
2022-09-11 21:52:37 -0500

    adding endpoints and routes about images-profile

--- src/routers/userRoutes.js
+++ src/routers/userRoutes.js
@@ -1,12 +1,15 @@
+require("dotenv").config();
 const express = require("express");
 const router = require("express").Router();
-const fs = require("fs");
-const util = require("util");
-const unlinkFile = util.promisify(fs.unlink);
 
 const multer = require("multer");
 const { uploadFile } = require("../lib/s3");
-const upload = multer({ dest: "uploads/" });
+
+const upload = multer({
+  dest: `https://${process.env.S3_BUCKET_NAME}.s3.${process.env.S3_BUCKET_REGION}.amazonaws.com`,
+});
+
+const ImageProfile = require("../models/userModel");
 
 router.get("/:key", (req, res) => {
   console.log(req.params);
@@ -16,14 +19,15 @@ router.get("/:key", (req, res) => {
   readStream.pipe(res);
 });
 
-router.post("/", upload.single("image"), async (req, res) => {
+router.post("/", upload.single("croppedImage"), async (req, res) => {
   const file = req.file;
   console.log(file);
   const result = await uploadFile(file);
-  await unlinkFile(file.path);
   console.log(result);
+  await ImageProfile.create({ photoUrl: req.file.path });
 
-  res.send({ imagePath: `/images/${result.Key}` });
+  // res.send({ imagePath: `/images/${result.Key}` });
+  res.status(200).json({ data: req.file.path });
 });
 
 module.exports = router;
